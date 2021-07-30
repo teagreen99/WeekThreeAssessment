eefaed782f15b7158283d637f668eddb861f9c86
Tim Green <timgreen99@gmail.com>
2021-07-30 17:11:36 -0600

    setting up

---
+++ WeekThreeAssessment/WeekThreeAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeekThreeAssessment
+//
+//  Created by Tim Green on 7/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

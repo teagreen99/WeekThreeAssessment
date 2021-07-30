c703ba0e3a143b32fb2b8ae0b96fb3e3d529eaea
Tim Green <timgreen99@gmail.com>
2021-07-30 17:17:18 -0600

    more setup

---
+++ WeekThreeAssessment/WeekThreeAssessment/CoreData/CoreDataStack.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataStack.swift
+//  WeekThreeAssessment
+//
+//  Created by Tim Green on 7/30/21.
+//
+
+import Foundation
---
+++ WeekThreeAssessment/WeekThreeAssessment/Helpers+Extensions/DateFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  DateFormatter.swift
+//  WeekThreeAssessment
+//
+//  Created by Tim Green on 7/30/21.
+//
+
+import Foundation
